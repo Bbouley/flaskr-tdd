3078609752b141b89dab9d4e0c955d285b0e98f7
bbouley <b.bouley@hotmail.co.uk>
2016-01-17 21:43:08 -0700

    added database functions

--- app.py
+++ app.py
@@ -11,5 +11,32 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 
+def connect_db():
+  """Connects to the database."""
+  rv = sqllite3.connect(app.config['DATABASE'])
+  rv.row_factory = sqlite3.Row
+  return rv
+
+# initialize DB
+def init_db():
+  with app.app_context():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+      db.cursor().executescript(f.read())
+    db.commit()
+
+# open db connection
+def get_db():
+  if not hasattr(g, 'sqlite_db'):
+    g.sqlite_db = connect_db()
+  return g.sqlite_db
+
+# close db connection
+@app.teardown_appcontext
+def close_db(error):
+  if hasattr(g, 'sqlite_db'):
+    g.sqlite_db.close
+
 if __name__ == '__main__':
+  init_db()
   app.run()
